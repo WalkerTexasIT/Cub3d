83641e1b030c0c1fec07c132b955407dc0db465b
Bryan Minner <bminner@w-r7-p5.s19.be>
2020-02-15 18:38:33 +0100

    tsb

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 
 int		printline(int x, int start, int end, t_ptr *ptr)
 {
+	//printf("%d, %d, %d\n", x, start, end);
 	while (start <= end)
 	{
 		mlx_pixel_put(ptr->mlx_ptr, ptr->win_ptr, x, start, 255);
@@ -41,27 +42,27 @@ int		algo(t_ptr *ptr, t_pos *pos)
 		pos->rayDirY = pos->dirY + pos->planeY * cameraX;
 		pos->mapX = (int)pos->posY;
 		pos->mapY = (int)pos->posX;
-		pos->deltaDistX = sqrt(1 + (pos->rayDirY * pos->rayDirY) / (pos->rayDirX * pos->rayDirX));
-		pos->deltaDistY = sqrt(1 + (pos->rayDirX * pos->rayDirX) / (pos->rayDirY * pos->rayDirY));
+		pos->deltaDistX = fabs(1 / pos->rayDirX);
+		pos->deltaDistY = fabs(1 / pos->rayDirY);
 		if (pos->rayDirX < 0)
 		{
 			pos->stepX = -1;
-			pos->sideDistX = (pos->posY - (double)pos->mapX) * pos->deltaDistX;
+			pos->sideDistX = (pos->posY - pos->mapX) * pos->deltaDistX;
 		}
 		else
 		{
 			pos->stepX = 1;
-			pos->sideDistX = ((double)pos->mapX + 1 - pos->posY) * pos->deltaDistX;
+			pos->sideDistX = (pos->mapX + 1.0 - pos->posY) * pos->deltaDistX;
 		}
 		if (pos->rayDirY < 0)
 		{
 			pos->stepY = -1;
-			pos->sideDistY = (pos->posX - (double)pos->mapY) * pos->deltaDistY;
+			pos->sideDistY = (pos->posX - pos->mapY) * pos->deltaDistY;
 		}
 		else
 		{
 			pos->stepY = 1;
-			pos->sideDistY = ((double)pos->mapY + 1 - pos->posX) * pos->deltaDistY;
+			pos->sideDistY = (pos->mapY + 1.0 - pos->posX) * pos->deltaDistY;
 		}
 		while (hit == 0)
 		{
@@ -77,23 +78,22 @@ int		algo(t_ptr *ptr, t_pos *pos)
 				pos->mapY += pos->stepY;
 				pos->side = 1;
 			}
-			printf("%d,%d\n", pos->mapX, pos->mapY);
 			if (ptr->map[pos->mapX][pos->mapY] == '1')
 				hit = 1;
 		}
-		printf("test\n");
 		if (pos->side == 0)
-			pos->perpWallDist = ((double)pos->mapX - pos->posY + (1 - pos->stepX) / 2) / pos->rayDirX;
+			pos->perpWallDist = ((double)pos->mapX - pos->posX + (1 - pos->stepX) / 2) / pos->rayDirX;
 		else
 			pos->perpWallDist = ((double)pos->mapY - pos->posY + (1 - pos->stepY) / 2) / pos->rayDirY;
 		pos->lineHeight = (int)((double)ptr->size_y / pos->perpWallDist);
+		if (x == 1)
+			printf("%f\n", pos->perpWallDist);
 		pos->drawStart = -pos->lineHeight / 2 + ptr->size_y / 2;
 		if (pos->drawStart < 0)
 			pos->drawStart = 0;
 		pos->drawEnd = pos->lineHeight / 2 + ptr->size_y / 2;
 		if (pos->drawEnd >= ptr->size_y)
 			pos->drawEnd = ptr->size_y - 1;
-		printf("test\n");
 		printline(x, pos->drawStart, pos->drawEnd, ptr);
 	}
 	return (0);
@@ -101,12 +101,12 @@ int		algo(t_ptr *ptr, t_pos *pos)
 
 int		init(t_ptr *ptr, t_pos *pos)
 {
-	//ptr->mlx_ptr = mlx_init();
-	//ptr->win_ptr = mlx_new_window(ptr->mlx_ptr, ptr->size_x, ptr->size_y, "CashGame");
+	ptr->mlx_ptr = mlx_init();
+	ptr->win_ptr = mlx_new_window(ptr->mlx_ptr, ptr->size_x, ptr->size_y, "CashGame");
 	/*if (mlx_hook(ptr->win_ptr, 2, 0, print, ptr) == -1)
 		return (-1);*/
 	algo(ptr, pos);
-	//mlx_loop(ptr->mlx_ptr);
+	mlx_loop(ptr->mlx_ptr);
 	return (0);
 }
 
